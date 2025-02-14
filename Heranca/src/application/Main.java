300cf945d43f11beb8e55fe37f6154d2b1c0319d
Marquin <marcus1224santos@hotmail.com.br>
2025-02-14 19:29:46 -0300

    override e polimorfismo

--- Heranca/src/application/Main.java
+++ Heranca/src/application/Main.java
@@ -31,6 +31,24 @@ public class Main {
         }
 
 
+        Account acco = new Account(1001, "Alex", 1000.0);
+        acco.withdraw(200.0);
+        System.out.println(acco.getBalance());
 
+        Account acco2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+        acco2.withdraw(200.0);
+        System.out.println(acco2.getBalance());
+
+        Account acco3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+        acco3.withdraw(200.0);
+        System.out.println(acco3.getBalance());
+
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
     }
 }
